b9689902bd62a50d9c1bb1fc4e949372cb48aa0a
Anton Nazarov <antonnaz@gmail.com>
2020-05-20 11:56:31 +0300

    Version bump, print_arr() is called

--- firstfile.py
+++ firstfile.py
@@ -2,6 +2,7 @@ import numpy as np
 def print_arr():
     for i in range(12):
         print(np.array([1,2,3]))
+print_arr()
 print("hi")
 print("Experiment is successful")
-print("Version 2.0")
+print("Version 2.1")
